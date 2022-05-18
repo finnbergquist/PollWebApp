26af5eec1edc25ffc58dd439060a60c7a379c78b
finnbergquist <fbergqui@bowdoin.edu>
2022-05-18 15:39:26 -0400

    names

--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_17_002725) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "bids", force: :cascade do |t|
-    t.string "bidder"
-    t.float "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
